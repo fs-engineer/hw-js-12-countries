207db83c84d8ac56718b8e086fd9a486570a36eb
Vadym <vad.evlanov@gmail.com>
2020-09-16 00:01:50 +0300

    add pnotify module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,64 @@
 import './styles.css';
-import './js/fetchCountries';
+
+import { alert, notice, info, success, error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+
+import { refs } from './js/refs';
+import fetchCountries from './js/fetchCountries';
+
+import countTemp from './templates/country.hbs';
+import fullCoutryInfo from './templates/full-country-info.hbs';
+
+const debounce = require('lodash.debounce');
+
+refs.searchQuery.addEventListener(
+  'input',
+  debounce(handleGenerateFromResponse, 500),
+);
+
+function handleGenerateFromResponse(event) {
+  let inputValue = getInputQuery(event);
+
+  if (inputValue) {
+    fetchCountries(inputValue);
+  }
+}
+
+function getInputQuery(event) {
+  return event.target.value;
+}
+
+function addCoutriesListToHTML(countriesMarkup) {
+  const markup = countTemp(countriesMarkup);
+
+  refs.countryWrapper.innerHTML = '';
+  refs.countryWrapper.insertAdjacentHTML('beforeend', markup);
+}
+
+function addFullCoutryInfo(country) {
+  const markup = fullCoutryInfo(country);
+
+  refs.countryWrapper.innerHTML = '';
+  refs.countryWrapper.insertAdjacentHTML('beforeend', markup);
+}
+
+export function selectTypeOutputInfo(numberOfCountries) {
+  numberOfCountries.length < 2
+    ? addFullCoutryInfo(numberOfCountries)
+    : numberOfCountries.length > 2 && numberOfCountries.length < 10
+    ? addCoutriesListToHTML(numberOfCountries)
+    : makePNotify();
+}
+
+function makePNotify() {
+  const myError = error({
+    text: 'Найдено слишком много совпадений, уточните ваш запрос',
+    type: 'error',
+    title: 'Ошибка',
+    styling: 'brighttheme',
+    icons: 'brighttheme',
+    animateSpeed: 'fast',
+    shadow: true,
+    closerHover: true,
+  });
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,54 +1,10 @@
-'use strict';
-import { refs } from './refs';
-import countTemp from '../templates/country.hbs';
-import fullCoutryInfo from '../templates/full-country-info.hbs';
+import { selectTypeOutputInfo } from '../index';
 
-const debounce = require('lodash.debounce');
-
-refs.searchQuery.addEventListener(
-  'input',
-  debounce(handleGenerateFromResponse, 500),
-);
-
-function handleGenerateFromResponse(event) {
-  let inputValue = getInputQuery(event);
-
-  if (inputValue) {
-    fetchCountries(inputValue);
-  }
-}
-
-function fetchCountries(searchQuery) {
+export default function fetchCountries(searchQuery) {
   fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
     .then(data => data.json())
     .then(countries => {
       selectTypeOutputInfo(countries);
     })
     .catch(error => console.log(error));
-}
-
-function getInputQuery(event) {
-  return event.target.value;
-}
-
-function addCoutriesListToHTML(countriesMarkup) {
-  const markup = countTemp(countriesMarkup);
-
-  refs.countryWrapper.innerHTML = '';
-  refs.countryWrapper.insertAdjacentHTML('beforeend', markup);
-}
-
-function addFullCoutryInfo(country) {
-  const markup = fullCoutryInfo(country);
-
-  refs.countryWrapper.innerHTML = '';
-  refs.countryWrapper.insertAdjacentHTML('beforeend', markup);
-}
-
-function selectTypeOutputInfo(numberOfCountries) {
-  numberOfCountries.length < 2
-    ? addFullCoutryInfo(numberOfCountries)
-    : numberOfCountries.length > 2 && numberOfCountries.length < 10
-    ? addCoutriesListToHTML(numberOfCountries)
-    : console.log('more 10');
 }
